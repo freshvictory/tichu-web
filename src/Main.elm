66d56d37f8401c5bf70f8159d700c65c29807e04
freshvictory <justin.renjilian@gmail.com>
2019-06-13 23:08:21 -0400

    A whole bunch of styling

--- src/Main.elm
+++ src/Main.elm
@@ -236,7 +236,7 @@ view model =
   if model.crashed then
     text "The app crashed :("
   else 
-    div [ css [ displayFlex, flexDirection column ] ]
+    div [ class "app" ]
       [ if (abs (model.vertScore - model.horzScore)) > 400 then
           button [ onClick CrashApp ] [ text "Things are not looking good" ]
         else
@@ -244,13 +244,13 @@ view model =
       , viewTeams model
       , viewTeamTurnScore model
       , viewConsecutiveVictory model
-      , button [ onClick Score ] [ text "score" ]
+      , button [ class "score", onClick Score ] [ text "Score" ]
       ]
 
 
 viewTeams : Model -> Html Msg
 viewTeams model =
-  div [ css [ displayFlex, flexDirection row ] ]
+  div [ class "teams" ]
     [ viewTeam model model.vertScore model.north model.south
     , viewTeam model model.horzScore model.east model.west
     ]
@@ -258,8 +258,8 @@ viewTeams model =
 
 viewTeam : Model -> Int -> (Player, Bet) -> (Player, Bet) -> Html Msg
 viewTeam model score player1 player2 =
-  div [ css [ displayFlex, flexDirection column ] ]
-    [ text (String.fromInt score)
+  div [ class "team" ]
+    [ div [ class "team-score"] [ text (String.fromInt score) ]
     , viewPlayer model player1
     , viewPlayer model player2
     ]
@@ -267,9 +267,7 @@ viewTeam model score player1 player2 =
 
 viewPlayer : Model -> (Player, Bet) -> Html Msg
 viewPlayer model playerBet =
-  div [ css [ displayFlex, flexDirection column ] ]
-    [ viewPlayerBet model playerBet
-    ]
+  viewPlayerBet model playerBet
 
 
 viewPlayerBet : Model -> (Player, Bet) -> Html Msg
@@ -277,31 +275,38 @@ viewPlayerBet model (player, bet) =
   let
     playerId = getPlayerId player
   in  
-    div [ css [ displayFlex, flexDirection column ] ]
-      [ div [ css [ displayFlex, flexDirection row ] ]
+    div [ class "bets" ]
+      [ div [ class "bet" ]
           [ labeledRadio
               ("none" ++ "-" ++ playerId)
               "None"
               ("bet" ++ "-" ++ playerId)
+              "bet-radio"
+              "bet-label"
               (bet == Zero)
               (ChangePlayerBet player Zero)          
           , labeledRadio
               ("tichu" ++ "-" ++ playerId)
               "Tichu"
               ("bet" ++ "-" ++ playerId)
+              "bet-radio"
+              "bet-label"
               (bet == Tichu)
               (ChangePlayerBet player Tichu)
           , labeledRadio
               ("grand" ++ "-" ++ playerId)
-              "Grand Tichu"
+              "Grand"
               ("bet"  ++ "-" ++ playerId)
+              "bet-radio"
+              "bet-label"
               (bet == GrandTichu)
               (ChangePlayerBet player GrandTichu)
           ]
       , if bet /= Zero then
-          div []
+          div [ class "success"]
             [ input
             [ type_ "checkbox"
+            , class "bet-success"
             , id ("success" ++ "-" ++ playerId)
             , Html.Styled.Attributes.checked (
               case model.firstOut of
@@ -310,7 +315,7 @@ viewPlayerBet model (player, bet) =
                 _ -> False 
             )
             , onCheck (ChangeFirstOut player) ] []
-            , label [ for ("success" ++ "-" ++ playerId) ] [ text "Successful" ]
+            , label [ class "bet-success-label", for ("success" ++ "-" ++ playerId) ] [ text "Successful" ]
             ]
         else
           text ""
@@ -319,56 +324,64 @@ viewPlayerBet model (player, bet) =
 
 viewTeamTurnScore : Model -> Html Msg
 viewTeamTurnScore model =
-  div [ css [ displayFlex, flexDirection row ] ]
-    [ text (String.fromInt model.vertTurnScore)
+  div [ class "turn-scores", css [ displayFlex, flexDirection row ] ]
+    [ div [ class "turn-score vert" ] [ text (String.fromInt model.vertTurnScore) ]
     , input
       [ type_ "range"
+      , class "range"
       , Html.Styled.Attributes.min "-25"
       , Html.Styled.Attributes.max "125"
       , value (String.fromInt model.vertTurnScore)
       , step "5"
       , onInput ChangeTeamScore ] []
-    , text (String.fromInt (100 - model.vertTurnScore))
+    , div [ class "turn-score horz" ] [ text (String.fromInt (100 - model.vertTurnScore)) ]
     ]
 
 
 viewConsecutiveVictory : Model -> Html Msg
 viewConsecutiveVictory model =
-  div [ css [ displayFlex, flexDirection row ] ]
-    [ input
-      [ type_ "checkbox"
-      , id "vert-cv"
-      , Html.Styled.Attributes.checked
-        (case model.firstOut of
-          Team (Vertical, _) -> True      
-          _ -> False
-        )
-      , onCheck (ConsecutiveVictory Vertical)
-      ][]
-    , label [ for "vert-cv" ] [ text "Consecutive" ]
-    , input
-      [ type_ "checkbox"
-      , id "horz-cv"
-      , Html.Styled.Attributes.checked
-        (case model.firstOut of
-          Team (Horizontal, _) -> True      
-          _ -> False
-        )
-      , onCheck (ConsecutiveVictory Horizontal)
-      ][]
-    , label [ for "horz-cv" ] [ text "Consecutive" ]
+  div [ class "consecutives", css [ displayFlex, flexDirection row ] ]
+    [ div [ class "consecutive"] 
+      [ input
+        [ type_ "checkbox"
+        , class "cv-check"
+        , id "vert-cv"
+        , Html.Styled.Attributes.checked
+          (case model.firstOut of
+            Team (Vertical, _) -> True      
+            _ -> False
+          )
+        , onCheck (ConsecutiveVictory Vertical)
+        ][]
+      , label [ class "cv-label", for "vert-cv" ] [ text "Consecutive" ]
+      ]
+    , div [ class "consecutive" ] 
+      [ input
+        [ type_ "checkbox"
+        , class "cv-check"
+        , id "horz-cv"
+        , Html.Styled.Attributes.checked
+          (case model.firstOut of
+            Team (Horizontal, _) -> True      
+            _ -> False
+          )
+        , onCheck (ConsecutiveVictory Horizontal)
+        ][]
+      , label [ class "cv-label", for "horz-cv" ] [ text "Consecutive" ]
+      ]
     ]
 
 
-labeledRadio : String -> String -> String -> Bool -> (String -> Msg) -> Html Msg
-labeledRadio elemid elemlabel rgroup isChecked msg =
+labeledRadio : String -> String -> String -> String -> String -> Bool -> (String -> Msg) -> Html Msg
+labeledRadio elemid elemlabel rgroup elemclass labelclass isChecked msg =
   div []
     [ input 
       [ type_ "radio"
+      , class elemclass
       , id elemid
       , name rgroup
       , Html.Styled.Attributes.checked isChecked
       , onInput msg
       ] []
-    , label [ for elemid ] [ text elemlabel ]
+    , label [ class labelclass, for elemid ] [ text elemlabel ]
     ]
