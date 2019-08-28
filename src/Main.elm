91b9d71a390289188cf0a6da102e07d0074b6717
freshvictory <justin.renjilian@gmail.com>
2019-08-27 23:03:31 -0400

    Test version bump to 0.0.7

--- src/Main.elm
+++ src/Main.elm
@@ -26,7 +26,7 @@ main : Program Json.Decode.Value Model Msg
 main =
   Browser.document
     { init = init
-    , view = \model -> { title = "Tichu 5", body = [ model |> view |> toUnstyled ] }
+    , view = \model -> { title = "Tichu 7", body = [ model |> view |> toUnstyled ] }
     , update = updateWithStorage
     , subscriptions = subscriptions
     }
